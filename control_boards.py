edcfec4d0ce354d89c2fa19bfd82cb1f0b310126
exe1099 <exe1099@exe1099.com>
2021-03-26 15:02:54 +0100

    Automatic initiating of boards

--- control_boards.py
+++ control_boards.py
@@ -1,20 +1,23 @@
 from Modules.tps_pmbus import TPS
 
-#  tps15 = TPS("15")
-#  tps15.load_defaults()
+try:
+    tps15 = TPS("15")
+    tps15.load_defaults()
 
-#tps16 = TPS("16")
-#tps16.load_defaults()
+    tps16 = TPS("16")
+    tps16.load_defaults()
 
-#  tps15 = TPS("1a")
-#  tps15.load_defaults()
+    tps15 = TPS("1a")
+    tps15.load_defaults()
 
-#  tps15 = TPS("1b")
-#  tps15.load_defaults()
+    tps15 = TPS("1b")
+    tps15.load_defaults()
 
-#  tps15 = TPS("1c")
-#  tps15.load_defaults()
+    tps15 = TPS("1c")
+    tps15.load_defaults()
 
-tps15 = TPS("15")
-tps15.load_defaults()
+    tps15 = TPS("16")
+    tps15.load_defaults()
 
+except IOError as e:
+    pass
